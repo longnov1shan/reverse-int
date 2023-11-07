49e70e0e2e930c3ab512d818913fc93928190ac0
longnov1shan <gurov91@gmail.com>
2023-11-07 21:15:41 +0100

    init:resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
   
+    let result = "";
+
+    let nVal=Math.abs(n);
+        for(let i = nVal.toString().length-1 ; i >= 0; i--) {
+        result += nVal.toString()[i];
+    }
+    console.log(result);
+
+    
+    return  Number(result);
+ 
+    
 }
